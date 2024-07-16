416303dbb480323d3a6c97d19734b72c22e01fab
Wei-Tang Chen <a2741890@gmail.com>
2024-07-16 22:34:10 +1000

    tidy up

--- src/NuGetReferenceSwitcher.Presentation/Views/MainDialog.xaml.cs
+++ src/NuGetReferenceSwitcher.Presentation/Views/MainDialog.xaml.cs
@@ -6,23 +6,21 @@
 // <author>Rico Suter, mail@rsuter.com</author>
 //-----------------------------------------------------------------------
 
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.PlatformUI;
+using MyToolkit.Collections;
+using MyToolkit.Mvvm;
+using NuGetReferenceSwitcher.Presentation.Models;
+using NuGetReferenceSwitcher.Presentation.ViewModels;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
-using EnvDTE;
-
-using Microsoft.VisualStudio.PlatformUI;
-
-using MyToolkit.Collections;
-using MyToolkit.Mvvm;
-using NuGetReferenceSwitcher.Presentation.Models;
-using NuGetReferenceSwitcher.Presentation.ViewModels;
 using Button = System.Windows.Controls.Button;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
-using Window = System.Windows.Window;
 
 namespace NuGetReferenceSwitcher.Presentation.Views
 {
@@ -30,15 +28,15 @@ namespace NuGetReferenceSwitcher.Presentation.Views
     public partial class MainDialog : DialogWindow
     {
         private OpenFileDialog _dlg;
-        
+
         /// <summary>Initializes a new instance of the <see cref="MainDialog"/> class. </summary>
         /// <param name="application">The application object. </param>
         /// <param name="extensionAssembly">The assembly of the extension. </param>
-        public MainDialog(DTE application, Assembly extensionAssembly)
+        public MainDialog(DTE2 application, Assembly extensionAssembly)
         {
             InitializeComponent();
 
-            Model.ExtensionAssembly = extensionAssembly; 
+            Model.ExtensionAssembly = extensionAssembly;
             Model.Application = application;
             Model.Dispatcher = Dispatcher;
 
